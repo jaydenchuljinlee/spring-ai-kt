53f50f6f89f3b337bd5d54adda5a24154b001b37
ironjin <cheoljin1234@naver.com>
2025-09-25 21:11:18 +0900

    [feature/chat-client] - Chat Client PromptTemplate 예제 추가

--- src/test/kotlin/com/example/ai/example/PromptTemplateTest.kt
+++ src/test/kotlin/com/example/ai/example/PromptTemplateTest.kt
@@ -3,7 +3,8 @@ package com.example.ai.example
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.ai.chat.client.ChatClient
-import org.springframework.ai.chat.prompt.Prompt
+import org.springframework.ai.chat.prompt.PromptTemplate
+import org.springframework.ai.template.st.StTemplateRenderer
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
@@ -12,12 +13,37 @@ class PromptTemplateTest {
     @Autowired
     lateinit var chatClientBuilder: ChatClient.Builder
 
-    @DisplayName("Prompt Template을 통한 예제")
+    @DisplayName("PromptTemplate render() 메서드를 사용한 예제")
     @Test
-    fun stringPromptTest() {
+    fun promptTemplateRenderTest() {
         val chatClient: ChatClient = chatClientBuilder.build()
 
-        val prompt = Prompt("Spring AI에 대해서 알려줄래?")
+        val promptTemplate = PromptTemplate.builder()
+            .renderer(StTemplateRenderer.builder()
+                .startDelimiterToken('<')
+                .endDelimiterToken('>').build()
+            )
+            .template("""
+                Spring AI의 <chapter> 대해서 알려줄래?
+            """.trimIndent())
+            .build()
+
+        val prompt = promptTemplate.render(mapOf("chapter" to "chatClient API"))
+
+        val response = chatClient.prompt(prompt)
+            .call()
+            .content();
+
+        println(response)
+    }
+
+    @DisplayName("PromptTemplate create() 메서드를 사용한 예제")
+    @Test
+    fun promptTemplateCreateTest() {
+        val chatClient: ChatClient = chatClientBuilder.build()
+
+        val promptTemplate = PromptTemplate("Spring AI에 대해서 알려줄래?")
+        val prompt = promptTemplate.create()
 
         val response = chatClient.prompt(prompt)
             .call()
